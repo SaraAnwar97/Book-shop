cc880e889bf32d3bfc0ac47e18896e7f8dcd5ccc
Sara Anwar <saraanwar97@gmail.com>
2021-05-01 01:42:22 +0200

    express.js behind scenes

--- Basic_node/app.js
+++ Basic_node/app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+//const http = require('http');
 const express = require('express');
 const app = express(); // initialize a new object where express.js will store and manager our app
 app.use((req,res,next)=>{
@@ -7,6 +7,8 @@ next(); // allows req to continue to next middleware in line , else send respons
 }); // add new middleware fn , fn will be executed for every req
 app.use((req,res,next)=>{
     console.log('In another middleware');
+    res.send('<h1>Hello</h1>');
     });
-const server = http.createServer(app); // app is a valid requestHandler
-server.listen(3000);
+//const server = http.createServer(app); // app is a valid requestHandler
+//server.listen(3000);
+app.listen(3000);
