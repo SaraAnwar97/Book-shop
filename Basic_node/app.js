ed9347863f42dab6b1639db48766824f6b262216
Sara Anwar <saraanwar97@gmail.com>
2021-05-01 01:00:47 +0200

    Adding middleware

--- Basic_node/app.js
+++ Basic_node/app.js
@@ -1,5 +1,12 @@
 const http = require('http');
 const express = require('express');
 const app = express(); // initialize a new object where express.js will store and manager our app
+app.use((req,res,next)=>{
+console.log('In the middleware');
+next(); // allows req to continue to next middleware in line , else send response
+}); // add new middleware fn , fn will be executed for every req
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    });
 const server = http.createServer(app); // app is a valid requestHandler
 server.listen(3000);
