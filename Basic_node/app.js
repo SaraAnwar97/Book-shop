640117a8d8eccbf46c470b7c20aa62619f040c40
Sara Anwar <saraanwar97@gmail.com>
2021-05-01 00:37:51 +0200

    install express.js

--- Basic_node/app.js
+++ Basic_node/app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const routes = require('./routes') // ./: cause routes is not a global module
-const server = http.createServer(routes.handler);
-console.log(routes.someText)
+const express = require('express');
+const app = express(); // initialize a new object where express.js will store and manager our app
+const server = http.createServer(app); // app is a valid requestHandler
 server.listen(3000);
