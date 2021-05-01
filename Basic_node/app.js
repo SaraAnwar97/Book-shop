ba3489925ee51af70c09e1a8e3b3739df5b01b41
Sara Anwar <saraanwar97@gmail.com>
2021-05-01 04:48:51 +0200

    parsing requests

--- Basic_node/app.js
+++ Basic_node/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express(); // initialize a new object where express.js will store and manager our app
-app.use('/',(req,res,next)=>{
-    console.log('This always works');
-    next();
-    });
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
 app.use('/add',(req,res,next)=>{
     console.log('In another middleware');
-    res.send('<h1>Hey</h1>'); // not calling next, so next middleware doesn't handle the request and to return only one response
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add</button></form>'); // not calling next, so next middleware doesn't handle the request and to return only one response
     });
+ app.use('/product',(req,res,next)=>{
+     console.log(req.body);
+     res.redirect('/');
+ })   
 // '/' is the path of the route , it means every route starts with /
 app.use('/',(req,res,next)=>{
-    console.log('In another middleware');
     res.send('<h1>Hello</h1>');
     });
 
