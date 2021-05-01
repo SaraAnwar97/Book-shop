f4c42b5a217c04a32fc0c3098ac1e695740a13ed
Sara Anwar <saraanwar97@gmail.com>
2021-05-01 02:55:08 +0200

    assignment2

--- Assignment2/app.js
+++ Assignment2/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
-app.use((req,res,next)=>{
+/*app.use((req,res,next)=>{
     console.log('hey');
 next();
 });
 app.use((req,res,next)=>{
     console.log('hello');
 res.send('<h1>Assignment2</h1>');
+});*/
+//most specific middleware first
+app.use('/users',(req,res,next)=>{
+    console.log('hello');
+    res.send('<h1>Middleware handles /users</h1>');
 });
+//less specific middleware second
+app.use('/',(req,res,next)=>{
+    console.log('hey');
+    res.send('<h1>Middleware handles /</h1>');
+});
+
 app.listen(3000);
