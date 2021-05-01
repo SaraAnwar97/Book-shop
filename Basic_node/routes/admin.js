bd5eb45f4f623908c85e5f182ee50dc729135a49
Sara Anwar <saraanwar97@gmail.com>
2021-05-02 01:21:48 +0200

    serve html pages to different routes

--- Basic_node/routes/admin.js
+++ Basic_node/routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 // /admin/add => GET
 router.get('/add-product',(req,res,next)=>{
     console.log('In another middleware');
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add</button></form>'); // not calling next, so next middleware doesn't handle the request and to return only one response
+   res.sendFile(path.join(__dirname,'../','views','add-product.html'));
     });
 // /admin/product => POST
  router.post('/product',(req,res,next)=>{
--- Basic_node/routes/shop.js
+++ Basic_node/routes/shop.js
@@ -1,7 +1,10 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 router.get('/',(req,res,next)=>{
-    res.send('<h1>Hello</h1>');
+    //__dir name is a global variable available by node.js , which holds the absolute on os to this proj folder
+    // '../' goes up one level from routes to views
+    res.sendFile(path.join(__dirname,'../','views','shop.html'))
     });
 
 module.exports = router;
