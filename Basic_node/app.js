b551a7a00a6dc1cec5aad28a10c059699ed1d1a3
Sara Anwar <saraanwar97@gmail.com>
2021-05-01 02:10:09 +0200

    handling different routes

--- Basic_node/app.js
+++ Basic_node/app.js
@@ -1,14 +1,17 @@
-//const http = require('http');
 const express = require('express');
 const app = express(); // initialize a new object where express.js will store and manager our app
-app.use((req,res,next)=>{
-console.log('In the middleware');
-next(); // allows req to continue to next middleware in line , else send response
-}); // add new middleware fn , fn will be executed for every req
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
+    console.log('This always works');
+    next();
+    });
+app.use('/add',(req,res,next)=>{
+    console.log('In another middleware');
+    res.send('<h1>Hey</h1>'); // not calling next, so next middleware doesn't handle the request and to return only one response
+    });
+// '/' is the path of the route , it means every route starts with /
+app.use('/',(req,res,next)=>{
     console.log('In another middleware');
     res.send('<h1>Hello</h1>');
     });
-//const server = http.createServer(app); // app is a valid requestHandler
-//server.listen(3000);
+
 app.listen(3000);
