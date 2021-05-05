82b769c39bb7d336f1f79d579302b9729e5812fe
Sara Anwar <saraanwar97@gmail.com>
2021-05-05 02:33:17 +0200

    install and implement Pug

--- Basic_node/routes/shop.js
+++ Basic_node/routes/shop.js
@@ -1,4 +1,4 @@
-const path = require('path');
+/*const path = require('path');
 const express = require('express');
 const rootDir = require('../util/path');
 const router = express.Router();
@@ -10,5 +10,19 @@ router.get('/',(req,res,next)=>{
     console.log('shop.js',adminData.products);
     res.sendFile(path.join(rootDir,'views','shop.html'))
     });
+
+module.exports = router;*/
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.render('shop');
+});
 
 module.exports = router;
