b811f08d6cc272978d7aa366d9db7104aa3914ed
Sara Anwar <saraanwar97@gmail.com>
2021-05-07 02:15:55 +0200

    Done

---
+++ Assignment4/app.js
@@ -0,0 +1,36 @@
+const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const users = [];
+
+
+app.set('view engine','ejs');
+app.set('views','views')
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/',(req,res,next)=>{
+res.render('index', {
+    pageTitle: 'Add user'
+});
+});
+
+app.get('/users',(req,res,next)=>{
+res.render('users',{
+    pageTitle: 'Users',
+    users: users,
+    hasUsers: users.length > 0
+
+});
+});
+
+app.post('/add-user',(req,res,next)=>{
+users.push({name: req.body.username});
+res.redirect('/users');
+
+});
+
+
+
+app.listen(3000);
