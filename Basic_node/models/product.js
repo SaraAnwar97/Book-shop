aa675dafec6d7bf919949c433975bcacf87d4869
Sara Anwar <saraanwar97@gmail.com>
2021-05-07 05:05:59 +0200

    refractored file storage code

--- Basic_node/models/product.js
+++ Basic_node/models/product.js
@@ -1,5 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const p  = path.join(path.dirname(process.mainModule.filename),
+'data', 
+'products.json');
+const getProductsFromFile = cb =>{
+        fs.readFile(p,(err,fileContent)=>{
+        if(err){
+            return cb([]);
+        }else{
+        cb(JSON.parse(fileContent)); // return file content as an array
+        }
+
+});
+};
+
 module.exports = class Product {
     constructor(title){
         this.title=title;
@@ -7,36 +21,17 @@ module.exports = class Product {
     }
 
     save() {
-        const p  = path.join(path.dirname(process.mainModule.filename),
-         'data', 
-         'products.json');
-
-        // read whole file at path p and either get an error or file content
-        fs.readFile(p,(err,fileContent) => {
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent); // .parse():takes incoming Json and returns arr/ object/ file content
-
-            }
+        getProductsFromFile(products =>{
             products.push(this);
             //.stringify(): takes js obj/ arr , converts it into Json 
             fs.writeFile(p,JSON.stringify(products), (err)=>{
                 console.log(err);
             });
-        }); 
+        });
     }
 
     static fetchAll(cb) {
-        const p  = path.join(path.dirname(process.mainModule.filename),
-         'data', 
-         'products.json');
-        fs.readFile(p,(err,fileContent)=>{
-        if(err){
-            cb([]);
-        }
-        cb(JSON.parse(fileContent)); // return file content as an array
-
-        });
+       getProductsFromFile(cb);
        
     }
 }
