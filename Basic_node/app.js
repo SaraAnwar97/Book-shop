8f2ca6ec0992c6e402e1d5d6f2fb319738ee747f
Sara Anwar <saraanwar97@gmail.com>
2021-05-22 17:16:18 +0200

    stored sessions in db

--- Basic_node/app.js
+++ Basic_node/app.js
@@ -1,11 +1,18 @@
+const path = require('path');
 const express = require('express');
 const bodyParser=require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const path = require('path');
-const app = express(); // initialize a new object where express.js will store and manage our app
+const mongoDBStore = require('connect-mongodb-session')(session);
 const errorController = require('./controllers/404');
 const User = require('./models/users');
+const MONGODB_URI = 'mongodb+srv://Sara:Ss4923@@@cluster0.ldpfv.mongodb.net/shop';
+const app = express();
+const store = new mongoDBStore({
+uri: MONGODB_URI,
+collection: 'sessions'
+});
+
 app.set('view engine','ejs'); // setting default template engine to ejs
 app.set('views','views');//where i am keeping my html files
 const adminRoutes = require('./routes/admin');
@@ -14,7 +21,13 @@ const authenticationRoutes = require('./routes/authentication');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public'))); // serving files statically so user can access them
-app.use(session({secret: 'my secret', resave:false, saveUninitialized:false}))
+app.use(session(
+  {secret: 'my secret', 
+  resave:false,
+  saveUninitialized:false,
+  store: store
+})
+);
 app.use((req, res, next) => {
   User.findById("60a1d3760fd0460b2f1cc4c5")
     .then(user => {
@@ -32,7 +45,7 @@ app.use(errorController.get404Page);
 //const url = 'mongodb+srv://Sara:Ss4923@cluster0.ldpfv.mongodb.net/shop?retryWrites=true&w=majority';
 //const url = 'mongodb+srv://Sara:Ss4923@@@cluster0.ldpfv.mongodb.net/shop?retryWrites=true&w=majority';
 mongoose
-.connect('mongodb+srv://Sara:Ss4923@@@cluster0.ldpfv.mongodb.net/shop?retryWrites=true&w=majority',{useNewUrlParser: true },{ useUnifiedTopology: true })
+.connect(MONGODB_URI,{useNewUrlParser: true },{ useUnifiedTopology: true })
 .then(result => {
   User.findOne().then(user => {
     if(!user){
