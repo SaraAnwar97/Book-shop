acab8cce5e33eda7d7d65d0a14a63f2b7a9a8e33
Sara Anwar <saraanwar97@gmail.com>
2021-06-06 00:29:14 +0200

    password validation

--- Basic_node/routes/authentication.js
+++ Basic_node/routes/authentication.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {check} = require('express-validator/check');
+const {check, body} = require('express-validator/check');
 const authenticationController = require('../controllers/authentication');
 const router = express.Router();
 
@@ -7,12 +7,19 @@ router.get('/login', authenticationController.getLogin);
 router.post('/login', authenticationController.postLogin);
 router.post('/logout',authenticationController.postLogout);
 router.get('/signup',authenticationController.getSignup);
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email').custom((value  , {req})=>{
+router.post('/signup', 
+[check('email').isEmail().withMessage('Please enter a valid email').custom((value  , {req})=>{
     if(value === 'test@test.com'){
         throw new Error('this email is forbidden');
     }
     return true;
-}),authenticationController.postSignup);
+}),
+body('password',
+'Please enter a password with only numbers , text , at least 5 characters and at most 10 characters'
+).isLength({min: 5, max:10}).isAlphanumeric()
+
+]
+,authenticationController.postSignup);
 router.get('/reset', authenticationController.getReset);
 router.post('/reset', authenticationController.postReset);
 router.get('/reset/:token', authenticationController.getNewPassword);
