26e97777fad5d1ce0b63c8137edc6946ba51e6ff
Sara Anwar <saraanwar97@gmail.com>
2021-06-15 18:19:19 +0200

    filter files using mimtype

--- Basic_node/app.js
+++ Basic_node/app.js
@@ -27,6 +27,17 @@ const fileStorage = multer.diskStorage({
 
 });
 
+const fileFilter = (req,file,cb) =>{
+  if(file.mimetype === 'image/png'||
+     file.mimetype === 'image/jpg'||
+     file.mimetype === 'image/jpeg'){
+       cb(null,true);
+     }else{
+       cb(null,false);
+     }
+
+};
+
 app.set('view engine','ejs'); // setting default template engine to ejs
 app.set('views','views');//where i am keeping my html files
 const adminRoutes = require('./routes/admin');
@@ -34,7 +45,7 @@ const shopRoutes = require('./routes/shop');
 const authenticationRoutes = require('./routes/authentication');
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(multer({storage: fileStorage}).single('image'));
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use(express.static(path.join(__dirname,'public'))); // serving files statically so user can access them
 app.use(session(
   {secret: 'my secret', 
