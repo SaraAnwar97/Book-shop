48e72491abbeacf92a877787b7e646499ab34207
Sara Anwar <saraanwar97@gmail.com>
2021-06-24 20:42:20 +0200

    fixed err

--- Basic_node/public/cssk/js/admin.js
+++
@@ -1,5 +0,0 @@
-const deleteProduct = (btn) =>{
-//console.log('clicked');
-const prodId = btn.parentNode.querySelector('[name = productId]').value;
-const csrf = btn.parentNode.querySelector('[name = _csrf]').value;
-};
