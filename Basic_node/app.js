bef495e631aa128950d713e535934b8f99794a15
Sara Anwar <saraanwar97@gmail.com>
2021-07-23 22:27:46 +0200

    setting up a SSL server

--- Basic_node/app.js
+++ Basic_node/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const https= require('https');
 const express = require('express');
 const bodyParser=require('body-parser');
 const mongoose = require('mongoose');
@@ -21,6 +22,8 @@ uri: MONGODB_URI,
 collection: 'sessions'
 });
 const csrfProtection = csrf();
+const privateKey = fs.readFileSync('server.key');
+const certificate = fs.readFileSync('server.cert');
 const fileStorage = multer.diskStorage({
   destination:(req,file,cb)=>{
     cb(null,'images');
@@ -117,7 +120,8 @@ app.use((error,req,res,next)=>{
 
 mongoose.connect(MONGODB_URI,{useNewUrlParser: true },{ useUnifiedTopology: true })
 .then(result => {
-  app.listen(process.env.PORT || 3000);
+  https.createServer({key:privateKey,cert:certificate},app)
+  .listen(process.env.PORT || 3000);
 })
 .catch(err => {
   console.log(err);
